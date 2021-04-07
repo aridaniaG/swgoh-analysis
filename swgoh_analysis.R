c1366dbfca766a718c85a783e3f267d3b779c8cf
Jarad Niemi <niemi@iastate.edu>
2021-04-07 10:47:18 -0500

    Add character shard drop probabilities plot

--- swgoh_analysis.R
+++ swgoh_analysis.R
@@ -8,28 +8,6 @@ load("swgoh_models.RData")
 # summary(m_Bayes_equipment)
 
 
-
-
-################################################################################
-# Character analysis 
-params = names(m_Bayes_character$coefficients)
-dc = as.data.frame(m_Bayes_character)
-
-
-# Extract posterior samples
-posterior_samples = data.frame(
-  intercept = dc[[params[1]]],
-  date      = dc[[params[2]]],
-  attempts  = dc[[params[3]]],
-  manual    = dc[[params[4]]],
-  
-  # Standard deviation of random effects
-  reward_sigma        = sqrt(dc[[155]]),
-  battle_sigma        = sqrt(dc[[154]]),
-  battle_reward_sigma = sqrt(dc[[153]])
-)
-
-
 density_plot = function(samples, name, lessthanzero = TRUE) {
   g = ggplot(data.frame(x = samples), aes(x = x)) +
     geom_density() 
@@ -38,7 +16,7 @@ density_plot = function(samples, name, lessthanzero = TRUE) {
   
   if (lessthanzero) {
     g = g + 
-      geom_area(data = d, aes(x=x, y=y), fill = "magenta", color = NA) +
+      geom_area(data = d, aes(x=x, y=y), fill = "#C8102E", color = NA) +
       geom_vline(xintercept = 0, linetype = "dashed") +
       geom_text(aes(x = Inf, y = Inf, hjust = 1.2, vjust = 1.2,
                     label = paste0("P < 0 = ", 
@@ -55,23 +33,124 @@ density_plot = function(samples, name, lessthanzero = TRUE) {
          y = "Probability density function") 
 }
 
-p1 = density_plot(posterior_samples$date, "Date")
-p2 = density_plot(posterior_samples$attempts, "Number of attempts")
-p3 = density_plot(posterior_samples$manual, "Manual indicator")
+################################################################################
+# Character analysis 
+params = names(m_Bayes_character$coefficients)
+dc = as.data.frame(m_Bayes_character)
 
-p4 = density_plot(posterior_samples$reward_sigma, "Reward random effect", FALSE)
-p5 = density_plot(posterior_samples$battle_sigma, "Battle random effect", FALSE)
-p6 = density_plot(posterior_samples$battle_reward_sigma, "Battle:reward random effect", FALSE)
+
+# Extract posterior samples
+character_samples = data.frame(
+  intercept = dc[[params[1]]],
+  date      = dc[[params[2]]],
+  attempts  = dc[[params[3]]],
+  manual    = dc[[params[4]]],
+  
+  # Standard deviation of random effects
+  reward_sigma        = sqrt(dc[[155]]),
+  battle_sigma        = sqrt(dc[[154]]),
+  battle_reward_sigma = sqrt(dc[[153]])
+)
+
+
+
+
+p1 = density_plot(character_samples$date, "Date")
+p2 = density_plot(character_samples$attempts, "Number of attempts")
+p3 = density_plot(character_samples$manual, "Manual indicator")
+
+p4 = density_plot(character_samples$reward_sigma, "Reward random effect", FALSE)
+p5 = density_plot(character_samples$battle_sigma, "Battle random effect", FALSE)
+p6 = density_plot(character_samples$battle_reward_sigma, "Battle:reward random effect", FALSE)
 
 pp = gridExtra::grid.arrange(p1, p2, p3, p4, p5, p6, nrow = 2)
 
 ggsave("character_posteriors.png", pp, width = 14, height = 12)
 
-# Posterior distributions
 
 
 
 
+################################################################################
+# Equipment analysis 
+de = as.data.frame(m_Bayes_equipment)
+
+
+# Extract posterior samples
+equipment_samples = data.frame(
+  intercept = de[[params[1]]],
+  date      = de[[params[2]]],
+  attempts  = de[[params[3]]],
+  manual    = de[[params[4]]],
+  
+  # Standard deviation of random effects
+  reward_sigma        = sqrt(de[[732]]),
+  battle_sigma        = sqrt(de[[731]]),
+  battle_reward_sigma = sqrt(de[[730]])
+)
+
+
+
+
+p1 = density_plot(equipment_samples$date, "Date")
+p2 = density_plot(equipment_samples$attempts, "Number of attempts")
+p3 = density_plot(equipment_samples$manual, "Manual indicator")
+
+p4 = density_plot(equipment_samples$reward_sigma, "Reward random effect", FALSE)
+p5 = density_plot(equipment_samples$battle_sigma, "Battle random effect", FALSE)
+p6 = density_plot(equipment_samples$battle_reward_sigma, "Battle:reward random effect", FALSE)
+
+pe = gridExtra::grid.arrange(p1, p2, p3, p4, p5, p6, nrow = 2)
+
+ggsave("equipment_posteriors.png", pe, width = 14, height = 12)
+
+
+
+# Specific combinations
+characters = c("Luke Skywalker (Farmboy)",
+               "Veteran Smuggler Han Solo",
+               "Rey (Scavenger)",
+               "Obi-Wan Kenobi (Old Ben)",
+               "Boba Fett",
+               "Clone Wars Chewbacca",
+               "Kylo Ren (Unmasked)",
+               "Kylo Ren","Young Lando Calrissian",
+               "Finn")
+
+pc = rewards %>% 
+  filter(reward %in% characters) %>%
+  select(battle, reward) %>%
+  unique() %>%
+  arrange(reward, battle) %>%
+  mutate(
+    date = as.Date("2019-09-15"),
+         attempts = 5,
+         manual = FALSE)
+
+lpc = posterior_epred(m_Bayes_character, 
+                        newdata = pc) 
+
+a = 0.05
+pc = pc %>% 
+  mutate(median = apply(lpc, 2, median),
+         lower  = apply(lpc, 2, function(x) quantile(x,   a/2)),
+         upper  = apply(lpc, 2, function(x) quantile(x, 1-a/2)),
+         
+         battle_reward = paste(battle,reward, sep=": "),
+         battle_reward = fct_reorder(battle_reward, median)) 
+
+pc_plot = ggplot(pc, aes(y = battle_reward, 
+               x = median, xmin = lower, xmax = upper,
+               color = reward)) +
+  geom_pointrange() +
+  labs(title = "Character shard drop probabilities",
+       y = "Battle: reward",
+       x = "Drop probabilities") + 
+  xlim(0,1) + 
+  theme(legend.position = "none")
+
+ggsave(file = "character_shard_drop_probabilities.png",
+       pc_plot, height = 10, width = 12)
 
 # Credible intervals
 plot(m_Bayes_character, "areas", prob = 0.95, 
